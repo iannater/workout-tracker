e6bdf1d978c21986d1f86d2bb2bb4e8b0ee6c59a
Ian Nater <ian.nater@gmail.com>
2020-11-18 21:55:56 -0600

    deploying

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
     useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
